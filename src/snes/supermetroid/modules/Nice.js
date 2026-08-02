fa64b51b5490a33bb65b91e0d5b430b81a85f41d
qwerinope <me@qwerinope.com>
2026-08-02 18:00:17 +0200

    six seven

--- src/snes/supermetroid/modules/Nice.js
+++ src/snes/supermetroid/modules/Nice.js
@@ -3,16 +3,16 @@ import Addresses from "../addresses";
 
 export default class NiceModule extends MemoryModule {
     constructor() {
-        super("niceEvent", "69...Nice");
-        this.tooltip = "Says 'Nice' when health is nice.";
+        super("niceEvent", "67 67 67 67 67");
+        this.tooltip = "Says '67 67 67 67 67' when health is 67.";
         this.reported = false;
-        this.lastNon69Time = Date.now();
+        this.lastNon67Time = Date.now();
         this.lastReport = 0;
     }
 
     settingDefs = {
         threshold: {
-            display: "Say 'Nice' if health is 69 for at least this many seconds",
+            display: "Say '67 67 67 67 67' if health is 67 for at least this many seconds",
             type: "number",
             default: 2,
         },
@@ -35,16 +35,16 @@ export default class NiceModule extends MemoryModule {
         const time = Date.now();
         if (
             !this.reported &&
-            memory.samusHP.value % 100 === 69 &&
-            time - this.lastNon69Time > this.settings.threshold.value * 1000 &&
+            memory.samusHP.value % 100 === 67 &&
+            time - this.lastNon67Time > this.settings.threshold.value * 1000 &&
             time - this.lastReport > this.settings.cooldown.value * 1000
         ) {
             this.reported = true;
             this.lastReport = Date.now();
-            sendEvent("msg", "Nice HP", 3);
+            sendEvent("msg", "67 67 67 67 67", 3);
         } else if (memory.samusHP.value % 100 !== 69) {
             this.reported = false;
-            this.lastNon69Time = time;
+            this.lastNon67Time = time;
         }
     }
 }
